fffa8ddc744cf71db73decbc93773e7b40c59e09
rachelandris <156002293+rachelandris@users.noreply.github.com>
2024-07-27 12:28:43 -0400

    Update CourseDAO.java
    put it back to original

--- app/src/main/java/com/gradecalculatorapp/repo/CourseDAO.java
+++ app/src/main/java/com/gradecalculatorapp/repo/CourseDAO.java
@@ -1,27 +1,5 @@
 package com.gradecalculatorapp.repo;
 
-import androidx.lifecycle.LiveData;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.Query;
-import androidx.room.Update;
-import java.util.List;
-
-@Dao
-public interface CourseDao {
-    @Insert
-    void insert(Course course);
-
-    @Update
-    void update(Course course);
-
-    @Delete
-    void delete(Course course);
-
-    @Query("DELETE FROM courses")
-    void deleteAllCourses();
-
-    @Query("SELECT * FROM courses")
-    LiveData<List<Course>> getAllCourses();
+public interface CourseDAO {
+    // TODO: Implement CourseDAO
 }
