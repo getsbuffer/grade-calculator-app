e25dd1a830ee323417f189cbe0105c71db18be94
rachelandris <156002293+rachelandris@users.noreply.github.com>
2024-07-27 12:32:58 -0400

    Update CourseDAO.java

--- app/src/main/java/com/gradecalculatorapp/repo/CourseDAO.java
+++ app/src/main/java/com/gradecalculatorapp/repo/CourseDAO.java
@@ -1,5 +1,24 @@
 package com.gradecalculatorapp.repo;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+import com.gradecalculatorapp.model.Course;
+import java.util.List;
+
+@Dao
 public interface CourseDAO {
-    // TODO: Implement CourseDAO
+    @Insert
+    void insert(Course course);
+
+    @Update
+    void update(Course course);
+
+    @Delete
+    void delete(Course course);
+
+    @Query("DELETE FROM courses")
+    void deleteAllCourses();
+
+    @Query("SELECT * FROM courses")
+    LiveData<List<Course>> getAllCourses();
 }
