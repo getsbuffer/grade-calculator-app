3161a923aa89395adff447b4cbf4a2e9991f6cce
rachelandris <156002293+rachelandris@users.noreply.github.com>
2024-07-27 12:33:21 -0400

    Update CourseDatabase.java

--- app/src/main/java/com/gradecalculatorapp/repo/CourseDatabase.java
+++ app/src/main/java/com/gradecalculatorapp/repo/CourseDatabase.java
@@ -1,7 +1,33 @@
 package com.gradecalculatorapp.repo;
 
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import com.gradecalculatorapp.model.Course;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Course.class}, version = 1)
+@TypeConverters(Converters.class)
 public abstract class CourseDatabase extends RoomDatabase {
-    //TODO: Implement CourseDatabase
+    private static CourseDatabase instance;
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public abstract CourseDAO courseDao();
+
+    public static synchronized CourseDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            CourseDatabase.class, "course_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
